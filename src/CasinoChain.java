ce17f89fc8ce94065604f923f1f87f885d6fb218
軒 <b02902016@ntu.edu.tw>
2016-01-10 17:26:03 +0800

    State Pattern Implementations for Casino functionality

---
+++ src/CasinoChain.java
@@ -0,0 +1,103 @@
+class CasinoChain{
+
+   private State m_current_state;
+
+   public static CasinoChain Init(){
+      return new CasinoChain();
+   }
+
+   private CasinoChain(){
+      m_current_state = new Init();
+   }
+
+   public void set_state(State s){
+      m_current_state = s;
+   }
+
+   public void pull(){
+      m_current_state.pull(this);
+   }
+}
+
+interface State{
+   void pull(CasinoChain wrapper);
+}
+
+class Init implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("Init");
+      wrapper.set_state(new Bet());
+      wrapper.pull();
+   }
+}
+
+class Bet implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("Bet");
+      wrapper.set_state(new Deal());
+      wrapper.pull();
+   }
+}
+
+class Deal implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("Deal");
+      wrapper.set_state(new Insurance());
+      wrapper.pull();
+   }
+}
+
+class Insurance implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("Insurance");
+      wrapper.set_state(new Surrender());
+      wrapper.pull();
+   }
+}
+
+class Surrender implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("Surrender");
+      wrapper.set_state(new PlayerAction());
+      wrapper.pull();
+   }
+}
+
+class PlayerAction implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("PlayerAction");
+      wrapper.set_state(new DealerAction());
+      wrapper.pull();
+   }
+}
+
+class DealerAction implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("DealerAction");
+      wrapper.set_state(new Result());
+      wrapper.pull();
+   }
+}
+
+class Result implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("Result");
+      wrapper.set_state(new End());
+      wrapper.pull();
+   }
+}
+
+class End implements State{
+
+   public void pull(CasinoChain wrapper){
+      System.out.println("End");
+   }
+}
